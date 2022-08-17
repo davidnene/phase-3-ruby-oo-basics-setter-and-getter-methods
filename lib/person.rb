74c054f309a16e2e17dfa373bee9d4afe2523c72
davidnene <davienesh4@gmail.com>
2022-08-17 17:32:33 +0300

    commit tree c22c405179ba692d6446411ea49b2f914161f150

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
+     #setter
+     def name=(name)
+        @name = name
+    end
+
+    #getter
+    def name
+        @name
+    end
+
+    #setter
+    def job=(job_name)
+        @job = job_name
+    end
+
+    #getter
+    def job
+        @job
+    end
 
 end
+
+ann = Person.new
+ann.job = "Mentor"
+
+puts ann.job
